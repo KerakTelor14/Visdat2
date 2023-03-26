3567cb10d044b7f0745a76c8e81dbfc27dec82e7
KerakTelor14 <128968310+KerakTelor14@users.noreply.github.com>
2023-03-26 19:58:06 +0700

    Update Codingan.py

--- Codingan.py
+++ Codingan.py
@@ -24,3 +24,20 @@ with st.expander("Eksplorasi Data"):
         (' population ', '  Area ', ' birthrate ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' gdppercap ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' lifeexpectatbirth ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' Country ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
     st.write('Size :', option_size)
 
+    option_color = st.selectbox(
+        '',
+        (' birthrate ', '  Area ', ' Country ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' gdppercap ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' lifeexpectatbirth ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' population ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
+    st.write('Color :', option_color)
+
+bub = st.slider('Ukuran Bubble', 1, 100, 60)
+df = pd.read_csv('factbook.csv')
+fig1 = px.scatter(df,
+    x=option_x,
+    y=option_y,
+    size=option_size,
+    color=option_color,
+    size_max=bub,
+    hover_data=[option_size],
+   color_continuous_scale=px.colors.sequential.Viridis,
+)
+st.plotly_chart(fig1)
